5881a236367e6552e692d44e6d863567c5be26a4
Mahm <marsonambinintsoa@gmail.com>
2024-06-30 00:11:07 +0300

    lecture sans expéditeur

--- lib/widgets/messages_widget.dart
+++ lib/widgets/messages_widget.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import '../models/messages.dart';
 import '../screens/direct_chat_screen.dart';
 import '../screens/group_chat_screen.dart';
 
 class ConversationWidget extends StatelessWidget {
   final Conversation conversation;
+  final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
 
-  const ConversationWidget({super.key, required this.conversation});
+  ConversationWidget({Key? key, required this.conversation}) : super(key: key);
 
   void _showOptions(BuildContext context) {
     showModalBottomSheet(
@@ -20,7 +22,6 @@ class ConversationWidget extends StatelessWidget {
               onTap: () {
                 Navigator.pop(context);
                 // Logique pour archiver la conversation
-
               },
             ),
             ListTile(
@@ -38,28 +39,109 @@ class ConversationWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(conversation.name),
-      subtitle: Text(conversation.lastMessage),
-      trailing: Text(
-        conversation.timestamp.toLocal().toString(),
-        style: const TextStyle(color: Colors.grey, fontSize: 12),
-      ),
-      onTap: () {
-        // Navigate to the correct chat screen based on conversation type
-        /*if (conversation.isGroup) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => GroupChatScreen(conversation: conversation)),
+    return FutureBuilder<String?>(
+      future: secureStorage.read(key: 'user'),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator(); // Ou n'importe quel autre indicateur de chargement
+        } else if (snapshot.hasData) {
+          final userId = snapshot.data!.replaceAll('"', '');
+          final isMessageSentByUser = conversation.dernierMessage.expediteur == userId;
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: conversation.contact.photo != null
+                  ? NetworkImage(conversation.contact.photo!)
+                  : null,
+              child: conversation.contact.photo == null ? const Icon(Icons.person) : null,
+            ),
+            title: Text(conversation.contact.nom),
+            subtitle: isMessageSentByUser ? _buildSentMessage(userId) : _buildReceivedMessage(userId),
+            trailing: Text(
+              conversation.dernierMessage.dateEnvoi.toLocal().toString(),
+              style: TextStyle(
+                color: _isUnread(userId) ? Colors.blue : Colors.grey,
+                fontSize: 12,
+                fontWeight: _isUnread(userId) ? FontWeight.bold : FontWeight.normal,
+              ),
+            ),
+            onTap: () => _navigateToChatScreen(context),
+            onLongPress: () => _showOptions(context),
           );
         } else {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => DirectChatScreen(conversation: conversation)),
-          );
-        }*/
+          return const SizedBox(); // Placeholder or loading indicator
+        }
       },
-      onLongPress: () => _showOptions(context),
     );
+  }
+
+  Widget _buildSentMessage(String userId) {
+    final message = conversation.dernierMessage;
+    final content = _getContentSubtitle(message.contenu);
+    final isRead = message is DernierMessageUtilisateur
+        ? (message as DernierMessageUtilisateur).lu
+        : (message as DernierMessageGroupe).luPar.any((lecture) => lecture.utilisateurId == userId);
+    return Text(
+      'Vous: $content',
+      style: TextStyle(
+        color: isRead ? Colors.grey : Colors.black,
+        fontWeight: isRead ? FontWeight.normal : FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget _buildReceivedMessage(String userId) {
+    final message = conversation.dernierMessage;
+    final content = _getContentSubtitle(message.contenu);
+    final isRead = message is DernierMessageUtilisateur
+        ? (message as DernierMessageUtilisateur).lu
+        : (message as DernierMessageGroupe).luPar.any((lecture) => lecture.utilisateurId == userId);
+    return Text(
+      content,
+      style: TextStyle(
+        color: isRead ? Colors.grey : Colors.black,
+        fontWeight: isRead ? FontWeight.normal : FontWeight.bold,
+      ),
+    );
+  }
+
+  String _getContentSubtitle(Contenu contenu) {
+    switch (contenu.type) {
+      case 'texte':
+        return contenu.texte ?? '';
+      case 'image':
+        return 'a envoyé une photo';
+      case 'audio':
+        return 'a envoyé un audio';
+      case 'video':
+        return 'a envoyé une vidéo';
+      default:
+        return 'a envoyé une pièce jointe';
+    }
+  }
+
+  void _navigateToChatScreen(BuildContext context) {
+/*     if (conversation.contact.type == "groupe") {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => GroupChatScreen(conversation: conversation)),
+      );
+    } else if (conversation.contact.type == "utilisateur") {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => DirectChatScreen(conversation: conversation)),
+      );
+    } */
+  }
+
+  bool _isUnread(String userId) {
+    if (conversation.dernierMessage is DernierMessageUtilisateur) {
+      DernierMessageUtilisateur message = conversation.dernierMessage as DernierMessageUtilisateur;
+      return message.expediteur != userId && !message.lu;
+    } else if (conversation.dernierMessage is DernierMessageGroupe) {
+      DernierMessageGroupe message = conversation.dernierMessage as DernierMessageGroupe;
+      // Vérifie si l'utilisateur en session n'a pas lu ce message dans le groupe
+      return message.expediteur != userId && !message.luPar.any((lecture) => lecture.utilisateurId == userId);
+    }
+    return false; // Par défaut, considéré comme lu si le type n'est pas reconnu
   }
 }
